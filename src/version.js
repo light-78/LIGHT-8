83676001170dff0a44f48839b53fa9a821c8d2ac
light-78 <88991329+light-78@users.noreply.github.com>
2021-08-16 07:30:58 -0300

    Add files via upload

---
+++ src/version.js
@@ -0,0 +1,18 @@
+const version = (prefix, pushname) => {
+    return `
+*LIGHT-BOT-8 🤖*
+
+*Actualizado:* 16 de agosto
+*Versión actual:* 8
+*Ofrecida por:* LIGHT
+
+Para conocer la ultima versión de FX-BOT-73 entra al siguiente blog
+
+
+
+by LIGHT
+`
+
+}
+
+exports.version = version
